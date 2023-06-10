5e0f74858b0841b7b27ba1d300aa3be60ecfc441
gcwhitfield <gwhitfieldjr@gmail.com>
2023-06-10 09:42:19 -0400

    Add 10 second timeout to request.

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ import readline
 
 FLAGS = flags.FLAGS
 
-_MODEL = flags.DEFINE_string("model", "text-davinci-003", "The model to use for generating completions.")
-_MAX_TOKENS = flags.DEFINE_integer("max_tokens", 4000, "The maximum number of tokens to generate.")
-_TEMPERATURE = flags.DEFINE_float("temperature", 0.0, "The temperature value for controlling randomness.")
+_MODEL = flags.DEFINE_string(
+    "model", "text-davinci-003", "The model to use for generating completions.")
+_MAX_TOKENS = flags.DEFINE_integer(
+    "max_tokens", 4000, "The maximum number of tokens to generate.")
+_TEMPERATURE = flags.DEFINE_float(
+    "temperature", 0.0, "The temperature value for controlling randomness.")
 
 _API_ENDPOINT = "https://api.openai.com/v1/completions"
+
 
 def query_chatgpt(prompt: str) -> None:
     api_key = os.environ.get("OPENAI_API_KEY")
@@ -30,29 +34,33 @@ def query_chatgpt(prompt: str) -> None:
     }
 
     response = requests.post(
-        _API_ENDPOINT, headers=headers, data=json.dumps(data))
+        _API_ENDPOINT, headers=headers, data=json.dumps(data), timeout=10)
     response_data = response.json()
     choices: list[str] = [
         choice["text"] for choice in response_data.get("choices", [])]
     output: str = "\n".join(choices)
     print(output)
 
+
 def main(argv):
 
-    readline.set_history_length(1000)  # Set the maximum number of history entries to store
-    readline.parse_and_bind("bind ^R em-inc-search-prev")  # Enable reverse search with CTRL+R
+    # Set the maximum number of history entries to store
+    readline.set_history_length(1000)
+    # Enable reverse search with CTRL+R
+    readline.parse_and_bind("bind ^R em-inc-search-prev")
 
     while True:
         user_input: str = input("ChatGPT: ")
-        
+
         if user_input.strip() == "":
-            continue   
-        
+            continue
+
         if user_input == "quit()":
             print("Exiting the program...")
             break
 
         query_chatgpt(user_input)
+
 
 if __name__ == "__main__":
     absl.app.run(main)
